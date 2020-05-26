e7ea91120e5b46051ac5c554f1b8e534a2ab1175
basil-kawak <basilalisa0@gmail.com>
2020-05-26 18:20:38 +0300

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 
-class Timer extends PureComponent {
+class Timer extends Component {
   constructor() {
     super();
     this.timer = React.createRef();
@@ -13,14 +13,14 @@ class Timer extends PureComponent {
   //Your code here
 
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if(this.state.time === nextState.time) {
-  //     return false
-  //   }else {
-  //     return true
-  //   }
+  shouldComponentUpdate(nextProps, nextState) {
+    if(this.state.time === nextState.time) {
+      return false
+    }else {
+      return true
+    }
     
-  // }
+  }
 
   componentDidUpdate() {
     this.timer.current.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
